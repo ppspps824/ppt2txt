55c9cdab8bdd4a12ef352baf003de314805114d8
papasim824 <example@example.com>
2023-07-10 21:44:53 +0900

    update

--- ppt2txt.py
+++ ppt2txt.py
@@ -5,8 +5,98 @@ from pathlib import Path
 
 import openai
 import streamlit as st
+from llama_index import download_loader
 from pptx import Presentation
 from pptx.enum.shapes import MSO_SHAPE_TYPE
+
+
+def reading_ppt(fp):
+    # ファイルの読み込み
+    prs = Presentation(fp)
+
+    # スライドごとに繰り返し
+    all_txt_list = []
+    for num, slide in enumerate(prs.slides):
+        # テキスト抽出
+        all_txt_list.append(f"## slide{num+1}\n")
+        all_txt_list = check_recursively_for_text(slide.shapes, all_txt_list)
+
+        # 発表者ノートからテキスト抽出
+        if slide.has_notes_slide and slide.notes_slide.notes_text_frame.text:
+            all_txt_list.append(slide.notes_slide.notes_text_frame.text)
+
+    all_text = "\n".join(all_txt_list)
+
+    return all_text
+
+
+# スライドから文字を抽出
+def check_recursively_for_text(this_set_of_shapes, txt_list):
+    for shape in order_shapes(this_set_of_shapes):
+        if shape.shape_type == MSO_SHAPE_TYPE.GROUP:
+            check_recursively_for_text(shape.shapes, txt_list)
+        else:
+            if hasattr(shape, "text"):
+                if shape.text:
+                    txt_list.append(f"{shape.text}\n")
+
+            if shape.has_table:  # 表に含まれるテキストを抽出
+                for cell in shape.table.iter_cells():
+                    txt_list.append(cell.text)
+
+    return txt_list
+
+
+def order_shapes(shapes):
+    return sorted(shapes, key=lambda x: (x.top, x.left))
+
+
+def reading_data(data, name):
+    check_name = name.lower()
+
+    if ".pdf" in check_name:
+        PDFReader = download_loader("PDFReader")
+        loader = PDFReader()
+        documents = loader.load_data(file=data)[0].text
+    elif any([".txt" in check_name, ".md" in name]):
+        MarkdownReader = download_loader("MarkdownReader")
+        loader = MarkdownReader()
+        documents = loader.load_data(file=data)[0].text
+    elif ".pptx" in check_name:
+        documents = reading_ppt(data)
+    elif ".docx" in check_name:
+        DocxReader = download_loader("DocxReader")
+        loader = DocxReader()
+        documents = loader.load_data(file=data)[0].text
+    elif any([".mp3" in check_name, ".mp4" in check_name]):
+        AudioTranscriber = download_loader("AudioTranscriber")
+        loader = AudioTranscriber()
+        documents = loader.load_data(file=data)[0].text
+    elif ".csv" in check_name:
+        PandasCSVReader = download_loader("PandasCSVReader")
+        loader = PandasCSVReader()
+        documents = loader.load_data(file=data)[0].text
+    elif "youtu" in check_name:
+        YoutubeTranscriptReader = download_loader("YoutubeTranscriptReader")
+        loader = YoutubeTranscriptReader()
+        documents = loader.load_data(ytlinks=[name])[0].text
+    elif "http" in check_name:
+        BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
+        loader = BeautifulSoupWebReader()
+        documents = loader.load_data(urls=[name])[0].text
+    # elif ext in [".png", ".jpeg", ".jpg"]:
+    #     ImageCaptionReader = download_loader("ImageCaptionReader")
+    #     loader = ImageCaptionReader()
+    #     documents = loader.load_data(file=data)
+    else:
+        try:
+            MarkdownReader = download_loader("MarkdownReader")
+            loader = MarkdownReader()
+            documents = loader.load_data(file=data)[0].text
+        except:
+            st.error(f"非対応のファイル形式です。：{name}")
+            st.stop()
+    return documents
 
 
 def think_answer(text, model):
@@ -69,55 +159,30 @@ def think_answer(text, model):
     st.stop()
 
 
-# スライドから文字を抽出
-def check_recursively_for_text(this_set_of_shapes, txt_list):
-    for shape in order_shapes(this_set_of_shapes):
-        if shape.shape_type == MSO_SHAPE_TYPE.GROUP:
-            check_recursively_for_text(shape.shapes, txt_list)
-        else:
-            if hasattr(shape, "text"):
-                if shape.text:
-                    txt_list.append(f"{shape.text}\n")
-
-            if shape.has_table:  # 表に含まれるテキストを抽出
-                for cell in shape.table.iter_cells():
-                    txt_list.append(cell.text)
-
-    return txt_list
-
-
-def order_shapes(shapes):
-    return sorted(shapes, key=lambda x: (x.top, x.left))
-
-
 if __name__ == "__main__":
 
     os.environ["OPENAI_API_KEY"] = st.secrets["OPEN_AI_KEY"]
     openai.api_key = st.secrets["OPEN_AI_KEY"]
 
     with st.sidebar:
-        file = st.file_uploader("")
+        file = None
+        url = ""
+        input_select = st.selectbox("読み込み形式を選択", ["File", "URL"])
 
-    if file:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            fp = Path(tmp_file.name)
-            fp.write_bytes(file.getvalue())
+        if input_select == "File":
+            file = st.file_uploader("File")
+            if file:
+                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                    fp = Path(tmp_file.name)
+                    fp.write_bytes(file.getvalue())
+                all_text = reading_data(fp, file.name)
+        else:
+            url = st.text_input("URL")
+            if url:
+                fp = url
+                all_text = reading_data(url, url)
 
-        # ファイルの読み込み
-        prs = Presentation(fp)
-
-        # スライドごとに繰り返し
-        all_txt_list = []
-        for num, slide in enumerate(prs.slides):
-            # テキスト抽出
-            all_txt_list.append(f"## slide{num+1}\n")
-            all_txt_list = check_recursively_for_text(slide.shapes, all_txt_list)
-
-            # 発表者ノートからテキスト抽出
-            if slide.has_notes_slide and slide.notes_slide.notes_text_frame.text:
-                all_txt_list.append(slide.notes_slide.notes_text_frame.text)
-
-        all_text = "\n".join(all_txt_list)
+    if any([file, url]):
         with st.expander(f"抽出結果:{file.name}"):
             st.code(all_text)
 
